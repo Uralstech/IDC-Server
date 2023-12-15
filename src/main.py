3eae5515f882adc042fe74c53517eff8ae247b67
Udayshankar Ravikumar <uralstech@gmail.com>
2023-12-16 04:02:35 +0530

    Updated max new tokens.

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, torch_dtype
 @app.post("/api/chat", response_model=ChatCompletionResult)
 async def ask(request: ChatCompletionsRequest):
     prompt = pipe.tokenizer.apply_chat_template(request.messages, tokenize=False, add_generation_prompt=True)
-    outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+    outputs = pipe(prompt, max_new_tokens=64, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
 
     full_reply: str = outputs[0]["generated_text"]
     index: int = full_reply.find("<|assistant|>")
